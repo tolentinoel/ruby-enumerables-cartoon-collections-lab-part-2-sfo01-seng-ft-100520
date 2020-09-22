e6767a86535728fa1f0d4b99557ebc2149fb9824
Ellaine Tolentino <rckrleyn@gmail.com>
2020-09-22 22:00:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ def long_planeteer_calls(planeteer_calls)
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   # Return the boolean result of this check
  if planeteer_calls.any? { |word| word.length >= 4 }
-  puts word
+  puts "HEWOOOOOO"
   end
 end
 
