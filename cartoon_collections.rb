ff453f2d62999b78a2ac7338466f16906d8eeeae
Ellaine Tolentino <rckrleyn@gmail.com>
2020-09-23 00:23:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,6 +17,6 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.find do |calls|
-     valid_calls.include?(calls)
+     valid_calls.any?(calls)
   end
 end
